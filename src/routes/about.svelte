2ce99c8ae64952fff85e6677d4c260da02d947ac
sam <samvbui1995@gmail.com>
2022-03-17 15:08:08 +1100

    sveltekit gameing guide website

---
+++ src/routes/about.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<div class="about">
+	<h2>About Us</h2>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, ullam recusandae iure aliquid
+		nobis quia, voluptatum molestias dolorum nostrum, incidunt nesciunt pariatur. Adipisci, magni
+		labore? Ipsam delectus a voluptate. Culpa impedit, architecto voluptatibus hic omnis dicta
+		excepturi expedita quos saepe non enim maiores voluptas nesciunt, voluptates tenetur molestiae
+		nulla odio, blanditiis libero ratione nihil dignissimos? Quasi ad reiciendis distinctio atque.
+	</p>
+	<a href="/guides">View Guides</a>
+	<a href="/">Home</a>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+</script>
+
+<div class="index">
+	<h2>some title</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nihil officiis perspiciatis animi
+		ad aspernatur non ducimus exercitationem mollitia fuga, quibusdam, ex corporis impedit quas vel
+		praesentium, velit facilis sapiente minus? Laudantium obcaecati dolorum earum facilis laborum
+		odit quasi! Provident maiores accusamus nam incidunt saepe culpa, aperiam recusandae praesentium
+		commodi assumenda placeat similique voluptate aliquid cum soluta odio, ea magnam?
+	</p>
+	<a href="/guides">View Guides</a>
+	<a href="/about">About</a>
+</div>
+
+<style>
+	.index {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
