4b99d05801593c94ea8f7d3a7a7de0c6edde4938
sam <samvbui1995@gmail.com>
2022-03-18 11:17:58 +1100

    generate guides

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Title from '$lib/Title.svelte';
+	import '../styles/global.css';
+</script>
+
+<header>
+	<Title title="S4mLab Gaming Guide" />
+</header>
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>Copyright 2022 S4mLab Gaming Guide</p>
+</footer>
+
+<style>
+	header {
+		display: flex;
+		justify-content: center;
+	}
+	main {
+		max-width: 960px;
+		margin: 20px auto;
+	}
+	footer {
+		text-align: center;
+	}
+</style>
--- src/routes/guides/index.svelte
+++ src/routes/guides/index.svelte
@@ -1,7 +1,35 @@
+<script context="module">
+	const postsUrl = 'https://jsonplaceholder.typicode.com/posts';
+	// load() is a function that svelte use to load data, you must use it in order to load external data
+	// svelte will automatically load any data for this component
+	export async function load({ fetcb }) {
+		const response = await fetch(postsUrl);
+		const guideObjsList = await response.json();
+
+		// reponse obj has ok prop, if true then the resquest is succeed, otherwise something went wrong
+		// and you neeed to return the data in this format
+		if (response.ok) {
+			return {
+				props: { guideObjsList }
+			};
+		}
+
+		return {
+			status: response.status,
+			error: new Error('Could not fetch the guides')
+		};
+	}
+</script>
+
+<script>
+	export let guideObjsList;
+</script>
+
 <div class="guides">
 	<ul>
-		<li><a href="/">guide 1</a></li>
-		<li><a href="/">guide 2</a></li>
+		{#each guideObjsList as guideObj}
+			<li><a href="/">{guideObj.title}</a></li>
+		{/each}
 	</ul>
 </div>
 
@@ -17,6 +45,6 @@
 		display: inline-block;
 		margin-top: 10px;
 		padding: 10px;
-		border: 1px dotted rgba(255, 255, 255, 0.2);
+		border: 1px dotted rgba(255, 255, 255, 0.1);
 	}
 </style>
